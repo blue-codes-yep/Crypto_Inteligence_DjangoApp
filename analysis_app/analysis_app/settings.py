671d889e346669edc9c0ae032c3b0a2e07e434f9
Blue <linzblue5@gmail.com>
2022-09-11 04:56:56 -0500

    fix

--- analysis_app/analysis_app/settings.py
+++ analysis_app/analysis_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2hhkx2aus**%6^&pdt$$948k=u!__*%ja_#s92=(e=1(q(2^co'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
